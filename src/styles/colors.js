e69d9718e554d7e8601170dc543cc69c95890abd
gbnam <gbnam453@gmail.com>
2025-05-20 20:25:01 +0900

    Style 목록 추가

---
+++ src/styles/colors.js
@@ -0,0 +1,20 @@
+export default {
+    // GrayScale
+    gray050: '#F7F7F7',
+    gray100: '#F3F3F2',
+    gray200: '#E6E5E5',
+    gray300: '#CDCCCB',
+    gray400: '#B4B3B1',
+    gray500: '#989997',
+    gray600: '#82807D',
+    gray700: '#686664',
+    gray800: '#4E4D4B',
+    gray900: '#343332',
+
+    // White/Black
+    white000: '#FDFDFC',
+    black000: '#1B1A18',
+
+    // Primary
+    primary001: '#F4BA40',
+};
---
+++ src/styles/textStyles.js
@@ -0,0 +1,107 @@
+import { StyleSheet } from 'react-native';
+import fonts from './fonts';
+
+export default StyleSheet.create({
+    // Title
+    title36Bold: {
+        fontFamily: fonts.bold,
+        fontSize: 36,
+        lineHeight: 38,
+    },
+    title32Bold: {
+        fontFamily: fonts.bold,
+        fontSize: 32,
+        lineHeight: 34,
+    },
+    title22Bold: {
+        fontFamily: fonts.bold,
+        fontSize: 22,
+        lineHeight: 24,
+    },
+    title20Bold: {
+        fontFamily: fonts.bold,
+        fontSize: 20,
+        lineHeight: 22,
+    },
+    Category: {
+        fontFamily: fonts.bold,
+        fontSize: 20,
+        lineHeight: 40,
+    },
+    title18Bold: {
+        fontFamily: fonts.bold,
+        fontSize: 18,
+        lineHeight: 22,
+    },
+    title18SemiBold: {
+        fontFamily: fonts.semiBold,
+        fontSize: 18,
+        lineHeight: 22,
+    },
+    title16SemiBold: {
+        fontFamily: fonts.semiBold,
+        fontSize: 16,
+        lineHeight: 19,
+    },
+    title16Bold: {
+        fontFamily: fonts.bold,
+        fontSize: 16,
+        lineHeight: 19,
+    },
+    title14Bold: {
+        fontFamily: fonts.bold,
+        fontSize: 14,
+        lineHeight: 21,
+    },
+
+    // Subtitle
+    subtitle20semiBold22: {
+        fontFamily: fonts.semiBold,
+        fontSize: 20,
+        lineHeight: 22,
+    },
+    subtitle18semiBold20: {
+        fontFamily: fonts.semiBold,
+        fontSize: 20,
+        lineHeight: 22,
+    },
+    subtitle14SemiBold21: {
+        fontFamily: fonts.semiBold,
+        fontSize: 14,
+        lineHeight: 21,
+    },
+    subtitle14SemiBold16: {
+        fontFamily: fonts.semiBold,
+        fontSize: 14,
+        lineHeight: 16,
+    },
+    subtitle12Medium14: {
+        fontFamily: fonts.medium,
+        fontSize: 12,
+        lineHeight: 14,
+    },
+    subtitle12Bold14: {
+        fontFamily: fonts.bold,
+        fontSize: 12,
+        lineHeight: 14,
+    },
+
+    // Body
+    body12Medium: {
+        fontFamily: fonts.medium,
+        fontSize: 12,
+        lineHeight: 14,
+    },
+
+    // Caption
+    caption14Medium16: {
+        fontFamily: fonts.medium,
+        fontSize: 14,
+        lineHeight: 16,
+    },
+    caption11Medium: {
+        fontFamily: fonts.medium,
+        fontSize: 11,
+        lineHeight: 13,
+    },
+});
