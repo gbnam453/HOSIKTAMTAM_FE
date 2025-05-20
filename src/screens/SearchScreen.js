4e4a8cb87399da6c0601e19f3e5786d7e756ad27
ansanghyeon <m95369114@gmail.com>
2025-05-20 21:56:04 +0900

    스플래쉬 화면 만듬

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,38 @@
-// src/screens/SearchScreen.js
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet, StatusBar } from 'react-native';
 
-const SearchScreen = ({ navigation }) => (
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('HomeScreen'); // 4초 뒤에 'HomeScreen'으로 이동
+    }, 4000);
+    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 클리어
+  }, []);
+
+  return (
     <View style={styles.container}>
-        <Text style={styles.title}>SearchScreen</Text>
-        <Button title="Go to List" onPress={() => navigation.navigate('DetailScreen')} />
-    </View>
-);
+      <StatusBar backgroundColor="#f8c830" barStyle="dark-content" />
 
-export default SearchScreen;
+      <Image
+        source={require('../assets/images/SplashScreen/logo.png')}
+        style={styles.logo}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
-    title: { fontSize: 24, marginBottom: 20 },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8c830',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
 });
+
+export default SplashScreen;
