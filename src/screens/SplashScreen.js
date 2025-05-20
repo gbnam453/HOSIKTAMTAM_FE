67de956b36c3d1f4a65b0c4c3047358271425e47
ansanghyeon <m95369114@gmail.com>
2025-05-20 21:53:15 +0900

    스플래쉬 화면 만듬

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,25 +1,38 @@
-// src/screens/SplashScreen.js
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, StatusBar } from 'react-native';
 
 const SplashScreen = ({ navigation }) => {
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            navigation.replace('HomeScreen');
-        }, 2000);
-        return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('HomeScreen'); // 4초 뒤에 'HomeScreen'으로 이동
+    }, 2000);
+    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 클리어
+  }, []);
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>호식탐탐 Splash</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#f8c830" barStyle="dark-content" />
+
+      <Image
+        source={require('../assets/images/SplashScreen/logo.png')}
+        style={styles.logo}
+      />
+    </View>
+  );
 };
 
-export default SplashScreen;
-
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
-    title: { fontSize: 32, fontWeight: 'bold' },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8c830',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
 });
+
+export default SplashScreen;
